a5e98aa10e5d6ce272aca265eeb64e8b9d0f33af
oedokumaci <oral.ersoy.dokumaci@gmail.com>
2023-04-03 15:55:20 +0300

    feat: add send_email

--- src/ta_workflow/utils.py
+++ src/ta_workflow/utils.py
@@ -1,7 +1,13 @@
 """Module for utility functions."""
 
 import logging
+import os
+import smtplib
 import sys
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from pathlib import Path
 from time import time
 from typing import Callable, ParamSpec, TypeVar
@@ -61,3 +67,82 @@ def timer_decorator(func: Callable[P, R]) -> Callable[P, R]:
         return result
 
     return wrapper
+
+
+def _mime_init(
+    from_addr: str, to_addr: list[str], subject: str, body: str
+) -> MIMEMultipart:
+    """Initializes the MIME object.
+
+    Args:
+        from_addr (str): from address
+        to_addr (list[str]): to address
+        subject (str): subject of the email
+        body (str): body of the email
+
+    Returns:
+        MIMEMultipart: MIME object
+    """
+
+    message = MIMEMultipart()
+
+    message["From"] = from_addr
+    message["To"] = ",".join(to_addr)
+    message["Subject"] = subject
+    message.attach(MIMEText(body, "plain"))
+    return message
+
+
+def send_email(
+    user: str,
+    password: str,
+    from_addr: str,
+    to_addr: list[str],
+    subject: str,
+    body: str,
+    files_path: list[str] | None = None,
+    server_name: str = "asmtp.bilkent.edu.tr",
+) -> None:
+    """Sends email to the given recipients.
+
+    Args:
+        user (str): username of the email account
+        password (str): password of the email account
+        from_addr (str): from address
+        to_addr (list[str]): to address
+        subject (str): subject of the email
+        body (str): body of the email
+        files_path (list[str] | None, optional): list of file paths to be attached, defaults to None
+        server (str, optional): server name, defaults to "asmtp.bilkent.edu.tr"
+    """
+
+    message = _mime_init(from_addr, to_addr, subject, body)
+
+    for file_path in files_path or []:
+        with open(file_path, "rb") as fp:
+            part = MIMEBase("application", "octet-stream")
+            part.set_payload((fp).read())
+            # Encoding payload is necessary if encoded (compressed) file has to be attached.
+            encoders.encode_base64(part)
+            part.add_header(
+                "Content-Disposition",
+                "attachment; filename= %s" % os.path.basename(file_path),
+            )
+            message.attach(part)
+
+    if server_name == "localhost":  # send mail from local server
+        # Start local SMTP server
+        server = smtplib.SMTP_SSL(server_name)
+        text = message.as_string()
+        server.send_message(message)
+    else:
+        # Start SMTP server at port 465
+        server = smtplib.SMTP_SSL(server_name, 465)
+        server.ehlo()
+        # Enter login credentials for the email you want to sent mail from
+        server.login(user, password)
+        text = message.as_string()
+        # Send mail
+        server.sendmail(from_addr, to_addr, text)
+
+    server.quit()
