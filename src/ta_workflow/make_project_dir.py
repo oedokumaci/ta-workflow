c8ebe4b18b052288ce1774c2b06c3d5265b276f5
oedokumaci <oral.ersoy.dokumaci@gmail.com>
2023-04-05 16:41:40 +0300

    fix: add quiz dir

--- src/ta_workflow/make_project_dir.py
+++ src/ta_workflow/make_project_dir.py
@@ -6,13 +6,13 @@ from ta_workflow.student import Student, parse_student_data
 PROJECT_ROOT = Path(YAML_CONFIG.project_root_path)
 
 
-def make_project_dir(students: list[Student], homework_names: list[str]) -> None:
+def make_project_dir(students: list[Student], assignment_names: list[str]) -> None:
     for student in students:
         student_dir = PROJECT_ROOT / (student.last_name + "_" + student.bilkent_id)
         student_dir.mkdir(exist_ok=True)
-        for homework in homework_names:
-            homework_dir = student_dir / homework
-            homework_dir.mkdir(exist_ok=True)
+        for assignment in assignment_names:
+            assignment_dir = student_dir / assignment
+            assignment_dir.mkdir(exist_ok=True)
 
 
 if __name__ == "__main__":
@@ -20,4 +20,6 @@ if __name__ == "__main__":
     HOMEWORKS_SO_FAR = [
         f"Homework_{i}" for i in range(1, YAML_CONFIG.number_of_homeworks + 1)
     ]
+    QUIZZES_SO_FAR = [f"Quiz_{i}" for i in range(1, YAML_CONFIG.number_of_quizzes + 1)]
     make_project_dir(STUDENTS, HOMEWORKS_SO_FAR)
+    make_project_dir(STUDENTS, QUIZZES_SO_FAR)
