bedc19bba30b24859f1e9042543b418189f8cff1
oedokumaci <oral.ersoy.dokumaci@gmail.com>
2023-05-06 20:09:35 +0300

    refactor: path.py for more readability

--- src/ta_workflow/path.py
+++ src/ta_workflow/path.py
@@ -2,5 +2,8 @@ from pathlib import Path
 
 from ta_workflow.config_parser import YAML_CONFIG
 
+# The root directory of the project
 PROJECT_ROOT = Path(YAML_CONFIG.project_root_path)
+
+# The path to the log directory
 LOG_PATH: Path = Path(__file__).parents[2] / "logs"
