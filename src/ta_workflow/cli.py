32e5d14c66b38c65e5ba94aee6f82019037e0e89
oedokumaci <oral.ersoy.dokumaci@gmail.com>
2023-05-06 20:08:15 +0300

    refactor: cli.py for more readability

--- src/ta_workflow/cli.py
+++ src/ta_workflow/cli.py
@@ -31,14 +31,18 @@ def distribute(
     """Distribute the assignments into their respective directories."""
     from ta_workflow.distribute_assignments import distribute_assignments
 
+    # Get the students and selected assignments.
     students, selected_assignments = get_students_and_selected_assignments("distribute")
 
     try:
         distribute_assignments(students, selected_assignments, copy, score_threshold)
     except FileNotFoundError:
+        # Log an error if the assignments directory is not found.
         logging.error(
             "Could not find the assignments directory. Run `make_dirs` first."
         )
+
+    # Log a message to indicate that the command has finished executing.
     logging.info("Distributing assignments finished.")
 
 
@@ -47,11 +51,15 @@ def excel(sym_link: bool = sym_link_option) -> None:
     """Create excel files for each assignment to be uploaded to AIRS."""
     from ta_workflow.grades_to_excel import grades_to_excel
 
+    # Get the students and selected assignments.
     students, selected_assignments = get_students_and_selected_assignments(
         "save to excel"
     )
 
+    # Call the function to create the excel files.
     grades_to_excel(students, selected_assignments, sym_link)
+
+    # Log a message to indicate that the command has finished executing.
     logging.info("Creating excel files finished.")
 
 
@@ -60,9 +68,13 @@ def make_dirs() -> None:
     """Create the directories for each student and assignment. If the directories already exist, it will not overwrite them."""
     from ta_workflow.make_project_dir import make_project_dir
 
+    # Get the students, homeworks, and quizzes.
     students, homeworks, quizzes = prepare()
 
+    # Call the function to create the directories.
     make_project_dir(students, homeworks + quizzes)
+
+    # Log a message to indicate that the command has finished executing.
     logging.info("Creating directories finished.")
 
 
@@ -71,7 +83,10 @@ def split_pdf() -> None:
     """Split a pdf file into individual pages."""
     from ta_workflow.pdf_splitter import get_input, split_pdf
 
+    # Get the file path and page numbers from the user.
     file_path, pages = get_input()
+
+    # Call the function to split the pdf file.
     split_pdf(file_path, pages)
     print("Splitting pdf finished.")  # logger not initialized for this module
 
