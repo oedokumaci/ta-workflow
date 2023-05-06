d7a996b6e42d267d666f9d7094c6ce1464c5dd26
oedokumaci <oral.ersoy.dokumaci@gmail.com>
2023-05-06 20:10:29 +0300

    refactor: utils.py for more readability

--- src/ta_workflow/utils.py
+++ src/ta_workflow/utils.py
@@ -17,11 +17,20 @@ from ta_workflow.config_parser import YAML_CONFIG
 from ta_workflow.path import LOG_PATH
 from ta_workflow.student import Student, parse_and_validate_student_data
 
+# Define type variables
 R = TypeVar("R")
 P = ParamSpec("P")
 
 
 def check_log_file_name(log_file_name: str) -> str:
+    """Check if the given log file name is valid and prompt the user to overwrite if necessary.
+
+    Args:
+        log_file_name (str): the name of the log file
+
+    Returns:
+        str: the valid log file name
+    """
     if not log_file_name.endswith(".log") and not log_file_name.endswith(".txt"):
         log_file_name = f"{log_file_name}.log"
 
@@ -40,32 +49,32 @@ def init_logger(log_file_name: str = "logs.log") -> None:
     """Initialize the logger.
 
     Args:
-        file_name (str): the name of the log file
+        log_file_name (str): the name of the log file
     """
+    # Get the valid log file name
     log_name = check_log_file_name(log_file_name)
     log_file: Path = LOG_PATH / log_name
+    # Delete existing log file and create a new one
     log_file.unlink(missing_ok=True)
     log_file.touch()
+    # Set up the log formatter
     log_formatter = logging.Formatter("%(asctime)s:%(levelname)s: %(message)s")
     log_formatter.datefmt = "%Y-%m-%d %H:%M:%S"
-
+    # Set up the log handlers
     log_handler = logging.FileHandler(str(log_file))
     log_handler.setFormatter(log_formatter)
     log_handler.setLevel(logging.INFO)
-
     std_log_handler = logging.StreamHandler(sys.stdout)
     std_log_handler.setFormatter(log_formatter)
     std_log_handler.setLevel(logging.DEBUG)
-
+    # Set up the logger
     logger = logging.getLogger()
     logger.addHandler(std_log_handler)
     logger.addHandler(log_handler)
     logger.setLevel(logging.DEBUG)
-
     # Set library logging level to error
     for key in logging.Logger.manager.loggerDict:
         logging.getLogger(key).setLevel(logging.ERROR)
-
     logging.info(f"Path to log file: {log_file.resolve()}")
 
 
@@ -135,9 +144,10 @@ def send_email(
         files_path (list[str] | None, optional): list of file paths to be attached, defaults to None
         server (str, optional): server name, defaults to "asmtp.bilkent.edu.tr"
     """
-
+    # Initialize the MIME object
     message = _mime_init(from_addr, to_addr, subject, body)
 
+    # Attach the files to the email if files_path is not None
     for file_path in files_path or []:
         with open(file_path, "rb") as fp:
             part = MIMEBase("application", "octet-stream")
@@ -169,7 +179,14 @@ def send_email(
 
 
 def prepare() -> tuple[list[Student], list[str], list[str]]:
+    """Prepares the necessary data for the application.
+
+    Returns:
+        tuple[list[Student], list[str], list[str]]: a tuple containing a list of Student objects, a list of homework names and a list of quiz names
+    """
+    # Parse and validate the student data from the CSV file
     STUDENTS: list[Student] = parse_and_validate_student_data()
+    # Create a list of homework names and quiz names
     HOMEWORKS_SO_FAR = [
         f"Homework_{i}" for i in range(1, YAML_CONFIG.number_of_homeworks + 1)
     ]
@@ -181,9 +198,19 @@ def prepare() -> tuple[list[Student], list[str], list[str]]:
 def get_students_and_selected_assignments(
     function_job: str,
 ) -> tuple[list[Student], list[str]]:
+    """Prompts the user to select homeworks/quizzes and returns a list of Student objects and a list of selected assignment names.
+
+    Args:
+        function_job (str): the job that the function is doing (e.g. grading)
+
+    Returns:
+        tuple[list[Student], list[str]]: a tuple containing a list of Student objects and a list of selected assignment names
+    """
+    # Prepare the necessary data
     students, homeworks, quizzes = prepare()
     assignments = homeworks + quizzes
     selected_assignments = []
+    # Prompt the user to select the assignments
     for assignment in assignments:
         if typer.confirm(f"Do you want to {function_job} {assignment}?"):
             selected_assignments.append(assignment)
