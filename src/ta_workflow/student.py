051281608e0735b6b59f6bcbf83cba409d394cd8
oedokumaci <oral.ersoy.dokumaci@gmail.com>
2023-05-06 18:53:26 +0300

    refactor: student.py add docstrings and comments

--- src/ta_workflow/student.py
+++ src/ta_workflow/student.py
@@ -1,4 +1,4 @@
-import pandas as pd  # type: ignore
+import pandas as pd
 from pydantic import BaseModel
 from unidecode import unidecode
 
@@ -7,6 +7,25 @@ from ta_workflow.path import PROJECT_ROOT
 
 
 class Student(BaseModel):
+    """
+    Represents a student in the system.
+
+    Attributes:
+    -----------
+    first_name : str
+        The first name of the student.
+    last_name : str
+        The last name of the student.
+    department : str
+        The department of the student.
+    bilkent_id : str
+        The unique identifier of the student.
+    email : str
+        The email address of the student.
+    withdraw_fz : bool
+        Whether the student has withdrawn from the course.
+    """
+
     first_name: str
     last_name: str
     department: str
@@ -20,12 +39,27 @@ class Student(BaseModel):
 
 
 def parse_and_validate_student_data(resave: bool = False) -> list[Student]:
+    """
+    Reads the student data from a file and validates it.
+
+    Parameters:
+    -----------
+    resave : bool, optional
+        Whether to save a fixed version of the file.
+
+    Returns:
+    --------
+    A list of Student objects representing the students in the file.
+    """
+
     try:
+        # Try to read the fixed Excel file first
         df = pd.read_excel(
             PROJECT_ROOT
             / (YAML_CONFIG.student_data_file_name.split(".")[0] + "_fixed.xlsx")
         )
     except FileNotFoundError:
+        # If the fixed Excel file does not exist, try to read the original file
         if YAML_CONFIG.student_data_file_name.endswith(".csv"):
             df = pd.read_csv(
                 PROJECT_ROOT / YAML_CONFIG.student_data_file_name, index_col=0
@@ -49,11 +83,13 @@ def parse_and_validate_student_data(resave: bool = False) -> list[Student]:
         df["first_name"] = df["first_name"].apply(unidecode)
         df["last_name"] = df["last_name"].apply(unidecode)
 
-    if resave:
-        df.to_excel(
-            PROJECT_ROOT
-            / (YAML_CONFIG.student_data_file_name.split(".")[0] + "_fixed.xlsx"),
-            index=False,
-        )
+        # If resave is True, save the fixed Excel file
+        if resave:
+            df.to_excel(
+                PROJECT_ROOT
+                / (YAML_CONFIG.student_data_file_name.split(".")[0] + "_fixed.xlsx"),
+                index=False,
+            )
 
+    # Create a list of Student objects from the data frame
     return [Student(**row._asdict()) for row in df.itertuples()]
